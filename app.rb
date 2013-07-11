9cbbcdc6e771c45d613350e21f85e108e8fbc7ba
Victor Asteinza <victor@theclymb.com>
2013-07-11 15:45:57 -0700

    working for the most part

--- app.rb
+++ app.rb
@@ -1,51 +1,48 @@
 require 'rubygems'
 require 'em-websocket'
-require 'yajl'
 require 'slim'
 require 'sinatra/base'
-require 'thin'
 require 'redis'
 
 $channel = EM::Channel.new
+SOCKETS = []
 
-EventMachine.run do
-  class App < Sinatra::Base
+Thread.new do
+  EventMachine.run do
+    class App < Sinatra::Base
 
-    get '/' do
-      slim :index
+      get '/' do
+        slim :index
+      end
     end
 
-    post '/' do
-      $channel.push "POST>: #{params[:text]}"
-    end
-  end
+    EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
+      ws.onopen do
+        sid = $channel.subscribe { |msg| ws.send msg }
+        $channel.push "[-122.926547,45.725029]"
+        puts "#{sid} connected"
+        puts "creating sockets"
+        SOCKETS << ws
+      end
 
-  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
-    ws.onopen do
-      sid = $channel.subscribe { |msg| ws.send msg }
-      $channel.push "[-122.926547,45.725029]"
-      puts "#{sid} connected"
-
-      #redis = Redis.new(:timeout => 0)
-      #redis.subscribe('mapdata') do |on|
-        #on.message do |channel, msg|
-          #data = JSON.parse(msg)
-          #puts "##{channel} -> #{msg}"
-          #$channel.push msg
-        #end
-      #end
-
-      ws.onmessage do |msg|
-        puts "received <#{sid}> #{msg}"
-        $channel.push "<#{sid}>: #{msg}"
-      end 
-
-      ws.onclose do 
+      ws.onclose do
         $channel.unsubscribe(sid)
-      end 
-    end 
+        puts "deleting socket"
+        SOCKETS.delete ws
+      end
+    end
 
+    App.run!({:port => 3000})
   end
-
-  App.run!({:port => 3000})
 end
+
+Thread.new do
+  redis = Redis.new
+  redis.subscribe('mapdata') do |on|
+    on.message do |channel, msg|
+      puts "##{channel} -> #{msg}"
+      $channel.push msg
+    end
+  end
+end
+sleep
