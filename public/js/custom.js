1ed9e55397e9aae7f80776cb361077b65468076f
dan terwilliger <dan.t@theclymb.com>
2013-07-12 12:03:24 -0700

    Removed Coordinates

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,17 +1,8 @@
 var doStuff = function (data) {
 	var blip, obj;
 	blip = new Beacon(data);
-	populateCoordList(blip.coords)
   populateLastItem(blip);
 	blip.createBlip();
-}
-
-var populateCoordList = function (coords) {
-	if ($("#coords p").length > 0){
-    $("#coords p:first").before('<p>' + coords + '</p>');
-  } else {
-    $("#coords").append('<p>' + coords + '</p>');
-  }
 }
 
 var populateLastItem = function (blip) {
