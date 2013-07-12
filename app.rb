0c83ecc69e472466fa20f9529e83d4c08aa0c0db
Victor Asteinza <victor@theclymb.com>
2013-07-11 17:53:02 -0700

    WIP: first swing at geocoding...needs some code clean up

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'em-websocket'
 require 'redis'
+require 'geocoder'
+require 'json'
 
 $channel = EM::Channel.new
 SOCKETS = []
-
 Thread.new do
   EventMachine.run do
     EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
@@ -24,10 +25,19 @@ Thread.new do
 end
 
 Thread.new do
+  Geocoder.configure(:lookup => :google)
+
   redis = Redis.new
   redis.subscribe('mapdata') do |on|
     on.message do |channel, msg|
+      data = JSON.parse(msg)
+      geocoded = Geocoder.search(data["address"])
+
+
+      data["coords"] = [geocoded[0].longitude, geocoded[0].latitude]
+      msg = JSON.generate(data)
       puts "##{channel} -> #{msg}"
+
       $channel.push msg
     end
   end
