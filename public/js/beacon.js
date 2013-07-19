35d2102a2366e3e800d121cbcc32e381432c7f5b
dan terwilliger <dan.t@theclymb.com>
2013-07-19 16:07:00 -0700

    Added price, collision detection, City name at bottom, expanding dots, and maths

--- public/js/beacon.js
+++ public/js/beacon.js
@@ -34,15 +34,18 @@ var Beacon = function (data) {
   this.coords = projection(data.coords);
 	this.imageUrl = data.imageUrl;
 	this.expire = new Date();
+	this.text = "+$" + Math.floor((Math.random()*250)+1).toString();
+	this.size = 5;
 	bl.beacons.push(this);
 	//bl.expireOldBeacons();
 };
 
 Beacon.prototype.createBlip = function () {
 	//Use D3 to create blip on map
-	coords = this.coords;
+	blip = this;
+	coords = blip.coords;
 	blipColor = "#00ccff";
-	pingColor = "#FF6633"
+	pingColor = "#FF6633";
 
 	//Draw ping
 	svg.append("circle")
@@ -50,46 +53,105 @@ Beacon.prototype.createBlip = function () {
 	  .attr("cy", coords[1])
 	  .attr("r", 2)
 	  .style("fill", pingColor)
-	  .style("opacity", .4)
+	  .style("opacity", .6)
 	  .style("stroke", pingColor)
 	  .style("stroke-opacity", 1)
-	  .style("stroke-width", 3)
+	  .style("stroke-width", 5)
 	  .transition()
-	  .duration(750)
-	  .attr("r", 72)
+	  .duration(950)
+	  .attr("r", 172)
 	  .style("opacity", 0)
 	  .style("stroke-opacity", 0)
 		.remove();
 
-	//Draw Circle
+	if(blip.text) {
+		svg.append("text")
+			.data(coords)
+			.attr("x", coords[0] - 45)
+	    .attr("y", coords[1])
+	    .attr("class","blip-text")
+	    .attr("fill", pingColor)
+	    .text( function (d) { return blip.text; })
+	    .transition()
+		  .duration(1500)
+		  .attr("x", coords[0] - 45)
+	    .attr("y", coords[1] - 90)
+		  .style("opacity", 0)
+		  .remove();
+	}
+
+	if(blip.imageUrl != "") {
+		//Draw image triangle
+	  svg.append('path')
+	    .attr('d', function(d) {
+	      var x = coords[0], y = coords[1] + 10;
+	      return 'M ' + x +' '+ y + ' l 10 10 l -20 0 z';
+	    })
+	    .attr("fill", blipColor)
+	    .transition()
+	    .duration(500)
+	    .delay(1000)
+	    .style("opacity", 0)
+	    .remove();
+
+	  //Draw image border box
+		svg.append("rect")
+			.attr("x", coords[0] - 41)
+	    .attr("y", coords[1] + 19)
+	    .attr("width", "82")
+	    .attr("height", "108")
+	    .attr("fill", blipColor)
+	    .transition()
+	    .duration(500)
+	    .delay(1000)
+	    .style("opacity", 0)
+	    .remove();
+
+	  //Draw image
+		svg.append("svg:image")
+	    .attr("xlink:href", this.imageUrl)
+	    .attr("x", coords[0] - 50)
+	    .attr("y", coords[1] + 22)
+	    .attr("width", "100")
+	    .attr("height", "100")
+	    .style("opacity", .90)
+	    .transition()
+	    .duration(500)
+	    .delay(1000)
+	    .style("opacity", 0)
+	    .remove();
+  }
+
+ 	//Draw Circle
 	svg.append("circle")
 		.data(coords)
 		.attr("cx", coords[0])
 		.attr("cy", coords[1])
-		.attr("r", 5)
+		.attr("r", blip.size)
 		.attr("class", "blip")
-		.style("fill", blipColor);
-
-	svg.append("svg:image")
-    .attr("xlink:href", this.imageUrl)
-    .attr("x", coords[0] - 20)
-    .attr("y", coords[1])
-    .attr("width", "100")
-    .attr("height", "100")
-    .style("stroke", blipColor)
-	  .style("stroke-opacity", 1)
-    .style("opacity", .86)
-    .transition()
-    .duration(450)
-    .delay(250)
-    .attr("height", "300")
-    .attr("width", "300")
-    .attr("x", coords[0] - 120)
-    .attr("y", coords[1] - 100)
-    .style("opacity", 0)
-    .remove();
-
+		.style("fill", blipColor)
+		.style("stroke", "#000000")
+		.style("stroke-width", 2);
 };
+
+Beacon.prototype.detectCollision = function(callback) {
+	var radians = [0.78, 1.57, 2.36, 3.14, 3.92, 4.71, 5.5, 0],
+			b = document.elementFromPoint(this.coords[0], this.coords[1]),
+			x, y;
+	if($(b).is("circle")) {
+		callback(this, b);
+	}
+	for(var i = 0; i < radians.length; i++) {
+		b = null;
+		dx = Math.cos(radians[i]) * this.size;
+		dy = Math.sin(radians[i]) * this.size;
+		b = document.elementFromPoint(this.coords[0] + dx, this.coords[1] + dy);
+		if($(b).is("circle")) {
+			callback(this, b);
+		}
+	}
+
+}
 
 // Beacon.prototype.destroy = function () {
 // 	blipData = bl.dataList();
--- public/js/custom.js
+++ public/js/custom.js
@@ -4,7 +4,41 @@ var doStuff = function (data) {
   populateLastItem(blip);
   populateSalesFeed(blip);
   $(".sales-counter").text(bl.beacons.length.toString());
+  $("#footer-text").text(blip.address);
+	blip.detectCollision(combineTouchingBlips);
 	blip.createBlip();
+	combineTouchingBlips(blip);
+}
+
+var combineTouchingBlips = function (blip, svgCircle) {
+	if(blip.size > 15 || svgCircle == undefined)
+		return;
+	var radius = parseInt($(svgCircle).attr("r")),
+		adjuster;
+	if(radius !== undefined) {
+		//Increase size by 1 past 20
+		if(radius >= 20) {
+			blip.size = radius + 1;
+		} else {
+			blip.size += radius;
+			//If two 15s join they can only become a 20.
+			if(blip.size >= 20)
+				blip.size = 20;
+		}
+		svgCoords = getCoordsFromSVGCircle(svgCircle);
+		//Set the new blips coordinates to the old one.
+		blip.coords = svgCoords;
+		$(svgCircle).remove();
+		blip.detectCollision(combineTouchingBlips);
+	}
+}
+
+//Get and return the coordsinates from an SVG circle objct
+var getCoordsFromSVGCircle = function (circle) {
+	var cx, cy;
+	cx = parseFloat($(circle).attr("cx"));
+	cy = parseFloat($(circle).attr("cy"));
+	return [cx, cy];
 }
 
 var populateLastItem = function (blip) {
